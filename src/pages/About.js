ecbef1a109eb4795dc244f499ca336083494e2f4
e-nk <enocklagat747@gmail.com>
2023-01-27 10:56:31 +0300

    done

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,11 +3,12 @@ import React from 'react'
 function About() {
     return (
         <div className="about-page">
-           <h4>Let’s Buy. <br></br> 
-We are seeking to develop a platform that makes it easy to search for products
- in most of that shops online all over the world. Your task is to bring the idea above to life in form of a platform. Using the following data: 
-</h4>
-           <p>Code is available at <a href="https://github.com/e-nk/phase2-project" target="blank">Github</a></p>    
+           <h4>Let’s Buy.</h4> <br></br> 
+<p>We are seeking to develop a platform that makes it easy to search for products
+ in most of that shops online all over the world. Your task is to bring the idea above to life in form of a platform. Using the following data: </p>
+
+           <p>Code is available at <a href="https://github.com/e-nk/phase2-project" target="blank">Github</a></p> 
+              
         </div>
     )
 }
