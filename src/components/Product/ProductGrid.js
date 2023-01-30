96c0348a6676fd8658ad090ca69563e7186da55a
e-nk <enocklagat747@gmail.com>
2023-01-30 20:21:00 +0300

    Search fixed

--- src/components/Product/ProductGrid.js
+++ src/components/Product/ProductGrid.js
@@ -1,22 +1,23 @@
-import React, {useContext} from 'react'
+import React, {useContext, useState} from 'react'
+
 import { StateContext } from '../../context/GlobalState'
 import ProductItem from './ProductItem'
 import './product.css'
+import Search from '../../context/Search/SearchItem'
 
 
 
 function ProductGrid() {
+    
+
     const {products} = useContext(StateContext)
+  console.log(products)
     
     return(
         <div>
+            {products && products.length > 0 && (<Search products={products}
+            />)}
             
-        <section className="products">
-
-            {
-                products.map((product) => <ProductItem key={product.id} product={product}/>)
-            }
-        </section>
         </div>
     ) 
 }
---
+++ src/context/Search/SearchItem.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import ProductItem from '../../components/Product/ProductItem'
+
+
+
+function Search({ products }) {
+  const [searchQuery, setSearchQuery] = useState("");
+  console.log("*******")
+  console.log(products);
+
+  const [searchResults, setSearchResults] = useState(products);
+  const handleSearch = (e) => {
+    setSearchQuery(e.target.value);
+    if (!products) {
+      return [];
+    }
+    let results = products.filter((product) => {
+      return (
+        product.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        product.department.toLowerCase().includes(searchQuery.toLowerCase()) 
+        // product.price.toString().includes(searchQuery)
+      );
+    });
+    setSearchResults(results);
+  };
+  return (
+    <div id="search">
+      <input
+        type="text"
+        placeholder="Search products"
+        onChange={handleSearch}
+        className="input"
+      />
+      <section className="products">
+        {searchResults.map((product) => (
+          <ProductItem key={product.id} product={product}/>
+        ))}
+      </section>
+    </div>
+  );
+}
+export default Search;
