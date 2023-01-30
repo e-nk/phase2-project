247c1d881cab4d7efc197a589fbf1549929fc945
e-nk <enocklagat747@gmail.com>
2023-01-30 20:24:23 +0300

    undefined usestate removed

--- src/components/Product/ProductGrid.js
+++ src/components/Product/ProductGrid.js
@@ -1,7 +1,5 @@
-import React, {useContext, useState} from 'react'
-
+import React, {useContext} from 'react'
 import { StateContext } from '../../context/GlobalState'
-import ProductItem from './ProductItem'
 import './product.css'
 import Search from '../../context/Search/SearchItem'
 
